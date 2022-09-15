4ca05da31e413fe8f4ff6e81448e440fabc2a461
vinayreddyyp <ypvinayreddy@gmail.com>
2022-09-14 22:08:32 -0700

    add validation to the app

---
+++ src/validations/validation.js
@@ -0,0 +1,25 @@
+const VALIDATOR_TYPE_REQUIRE = "REQUIRE";
+const VALIDATOR_TYPE_MINLENGTH = "MINLENGTH";
+
+export const VALIDATOR_REQUIRE = () => ({
+	type: VALIDATOR_TYPE_REQUIRE,
+});
+export const VALIDATOR_MINLENGTH = (value) => ({
+	type: VALIDATOR_TYPE_MINLENGTH,
+	val: value,
+});
+
+export const validation = (value, validators) => {
+	let isValid = true;
+	for (let validator of validators) {
+		if (validator.type === VALIDATOR_TYPE_REQUIRE) {
+			debugger;
+			isValid = isValid && value.trim().length > 0;
+		}
+		if (validator.type === VALIDATOR_TYPE_MINLENGTH) {
+			isValid = isValid && value.trim("").length >= validator.val;
+		}
+	}
+	console.log("isValid", isValid);
+	return isValid;
+};
